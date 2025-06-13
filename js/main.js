d82580e5714e55d87210270d6441407df84f9c82
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-06-13 09:23:46 -0300

    Remoção de codigo não utilizado

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ let midiOutput = null;
 const logElem = document.getElementById('log');
 const midiOutSelect = document.getElementById('midi-out-select');
 const connectBtn = document.getElementById('connect-btn');
-const sendLedBtn = document.getElementById('send-led-btn');
-const apagar = document.getElementById('apagar');
 
 function log(msg) {
     logElem.textContent += msg + '\n';
@@ -32,27 +30,24 @@ function updateMidiOutputs() {
     if (midiOutSelect.options.length > 0) {
         midiOutSelect.selectedIndex = 0;
         midiOutput = midiAccess.outputs.get(midiOutSelect.value);
-        sendLedBtn.disabled = false;
-        apagar.disabled = false;
-        log('Saída MIDI selecionada: ' + midiOutput.name);
-    } else {
-        sendLedBtn.disabled = true;
-        log('Nenhuma saída MIDI encontrada.');
+        log('Saída MIDI selecionada: ' + midiOutput.name + "\n");
+        } else {
+            log('Nenhuma saída MIDI encontrada.');
+        }
     }
-}
-
-connectBtn.addEventListener('click', () => {
-    if (!navigator.requestMIDIAccess) {
-        log('Web MIDI API não suportada no seu navegador.');
+    
+    connectBtn.addEventListener('click', () => {
+        if (!navigator.requestMIDIAccess) {
+            log('Web MIDI API não suportada no seu navegador.');
         return;
     }
     navigator.requestMIDIAccess({ sysex: true })
-        .then(onMIDISuccess, onMIDIFailure);
+    .then(onMIDISuccess, onMIDIFailure);
 });
 
 midiOutSelect.addEventListener('change', () => {
     midiOutput = midiAccess.outputs.get(midiOutSelect.value);
-    log('Saída MIDI alterada para: ' + midiOutput.name);
+    log('Saída MIDI alterada para: ' + midiOutput.name + "\n");
 });
 
 window.addEventListener('keydown', (e) => {
@@ -63,27 +58,6 @@ window.addEventListener('keyup', (e) => {
     const tecla = keyMapUp[e.keyCode];
     if (tecla) alteraPads(tecla)
 })
-
-// ---------------------------------------------------------
-
-// const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-const btnLetras = document.querySelectorAll('.btnTeste')
-const tecladoDiv = document.getElementById('teclado');
-
-btnLetras.forEach(btn => {
-    var msg = 'letra' + btn.textContent
-    btn.addEventListener('touchStart', () => {
-        console.log(msg)
-        midiOutput.send(msg)
-    });
-})
-
-// letras.forEach(letra => {
-//     const btn = document.createElement('button');
-//     btn.textContent = letra;
-//     btn.addEventListener('click', () => midiOutput.send(window['mobile' + letra]));
-//     tecladoDiv.appendChild(btn);
-// });
 
 function alteraPads(array) {
     array.forEach(letra => {
